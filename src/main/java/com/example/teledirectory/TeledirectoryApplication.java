1bc50629753c956d6bfccc71ffe9ddf602804460
dipika992 <dipikhan24@gmail.com>
2023-02-15 10:31:41 +0530

    firstproject

---
+++ src/main/java/com/example/teledirectory/TeledirectoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.teledirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeledirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeledirectoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/teledirectory/TeledirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teledirectory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeledirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
